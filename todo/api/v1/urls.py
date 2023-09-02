d1626d3d6214984f919123e445fd78d1363a204d
abbasalirezaei <abbasalirezaei.79@gmail.com>
2023-09-02 16:21:22 +0330

    a few change in urls and delete extra things.

--- todo/api/v1/urls.py
+++ todo/api/v1/urls.py
@@ -4,12 +4,4 @@ from rest_framework.routers import SimpleRouter
 
 router=SimpleRouter()
 router.register('task-list',views.TaskListViewSets,basename='task-list')
-# router.register("task-list", ciewTaskListViewSets, basename="task_list")
-# router.register(
-#     "task-detail",
-#     views.TaskDetailViewSets,
-#     basename="task_detail",
-# )
-# router.register('category',views.CategoryModelViewSet,basename='category')
-
 urlpatterns=router.urls
